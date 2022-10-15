93e97fa68494d1573094e74c4a93aa236fa16708
Alper alanyalie <alanyalialper@gmail.comm>
2022-10-15 18:12:09 +0300

    Add existing file

---
+++ users.js
@@ -0,0 +1,13 @@
+module.exports.getUserList = ()=> {
+
+          return [
+              {
+                  id:1,
+                  name:"Alper"
+              },
+              {
+                  id:2,
+                  name:"Mert"
+              }
+          ]
+}
